c300d5ad973e534eab9c3a84a735694456638d30
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-25 11:29:32 +0200

    Avoid duplicate export-name literals in layout test

--- tests/test_bayescatrack_layout.py
+++ tests/test_bayescatrack_layout.py
@@ -4,25 +4,13 @@ import bayescatrack
 from bayescatrack import association
 from bayescatrack import io as bayescatrack_io
 from bayescatrack import reference, registration, track2p_registration
+from bayescatrack.core import bridge as bayescatrack_bridge
 from bayescatrack.datasets import track2p as bayescatrack_track2p
 from tests._support import run_module
 
 
 def test_root_package_exports_expected_public_api():
-    expected_names = {
-        "CalciumPlaneData",
-        "SessionAssociationBundle",
-        "Track2pSession",
-        "build_consecutive_session_association_bundles",
-        "build_session_pair_association_bundle",
-        "export_subject_to_npz",
-        "find_track2p_session_dirs",
-        "load_raw_npy_plane",
-        "load_suite2p_plane",
-        "load_track2p_subject",
-        "main",
-        "summarize_subject",
-    }
+    expected_names = set(bayescatrack_bridge.__all__)
     assert expected_names.issubset(set(bayescatrack.__all__))
 
 
