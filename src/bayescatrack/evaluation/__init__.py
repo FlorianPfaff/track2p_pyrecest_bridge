4390b1ddad48c50fb4596920ae1f20270fe916fc
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-01 11:08:45 +0200

    Export identity-switch diagnostics

--- src/bayescatrack/evaluation/__init__.py
+++ src/bayescatrack/evaluation/__init__.py
@@ -1,16 +1,19 @@
 """Evaluation helpers for BayesCaTrack benchmarks."""
 
 from . import complete_track_scores as _scores
+from . import identity_switch_scores as _identity_switch_scores
 from . import track2p_metrics as _track2p_metrics
 
 complete_track_set = _scores.complete_track_set
+identity_switch_events = _identity_switch_scores.identity_switch_events
 normalize_track_matrix = _scores.normalize_track_matrix
 pairwise_track_set = _scores.pairwise_track_set
 score_complete_tracks = _scores.score_complete_tracks
+score_identity_switches = _identity_switch_scores.score_identity_switches
 score_pairwise_tracks = _scores.score_pairwise_tracks
 score_track_matrices = _scores.score_track_matrices
 summarize_tracks = _scores.summarize_tracks
 track_lengths = _scores.track_lengths
 score_track_matrix_against_reference = _track2p_metrics.score_track_matrix_against_reference
 
-__all__ = [*_scores.__all__, "score_track_matrix_against_reference"]
+__all__ = [*_scores.__all__, *_identity_switch_scores.__all__, "score_track_matrix_against_reference"]
