3182d8f4726fd69e4d1748467cd520790b53ef0d
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-01 12:14:29 +0200

    Export false continuation metric

--- src/bayescatrack/evaluation/__init__.py
+++ src/bayescatrack/evaluation/__init__.py
@@ -7,10 +7,11 @@ complete_track_set = _scores.complete_track_set
 normalize_track_matrix = _scores.normalize_track_matrix
 pairwise_track_set = _scores.pairwise_track_set
 score_complete_tracks = _scores.score_complete_tracks
+score_false_continuations = _scores.score_false_continuations
 score_pairwise_tracks = _scores.score_pairwise_tracks
 score_track_matrices = _scores.score_track_matrices
 summarize_tracks = _scores.summarize_tracks
 track_lengths = _scores.track_lengths
 score_track_matrix_against_reference = _track2p_metrics.score_track_matrix_against_reference
 
-__all__ = [*_scores.__all__, "score_track_matrix_against_reference"]
+__all__ = list(_scores.__all__) + ["score_track_matrix_against_reference"]
